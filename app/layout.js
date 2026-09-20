dcddb49e24316cf3c842b068efa6bf58defb2b07
Ahmed Magdy <ahmed@example.com>
2026-09-20 09:58:15 +0300

    update

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,6 @@ import WhatsApp from "./components/WhatsApp/WhatsApp";
 export const metadata = {
   title: "Ahmed's Portfolio",
   description: "Ahmed's Portfolio Fronend Developer",
-  icon: [
-    { url: "/assets/portfolio.jpeg" }, // المسار الاحتياطي من مجلد public
-  ],
-  shortcut: ["/assets/portfolio.jpeg"],
-  apple: ["/assets/portfolio.jpeg"],
 };
 
 export default function RootLayout({ children }) {
